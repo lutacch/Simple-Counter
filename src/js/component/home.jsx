c159d86e41708ad5a099fcf79f7c52c6c0287170
lutacch <lu.tacchetti@gmail.com>
2022-10-17 13:07:47 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Card from  "/workspace/react-hello/src/js/component/card.jsx";
+import Card from  "/workspace/Simple-Counter/src/js/component/card.jsx";
 
 //include images into your bundle
-
 
 //create your first component
 const Home = () => {
