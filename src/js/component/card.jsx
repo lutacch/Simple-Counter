5776b6ec695a1fd20dd7072a967f913cb2b91bfc
lutacch <lu.tacchetti@gmail.com>
2023-03-02 15:27:01 +0000

    update

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 
 
 
-function Card(){
+function Card(props){
     
   const [counterSeconds1, setCounter1] = useState(0);
   const [counterSeconds2, setCounter2] = useState(0);
@@ -13,90 +13,67 @@ function Card(){
   const [counterHours2, setCounter6] = useState(0);
  
   useEffect(()=> {
-     
-      const miTimeout = setInterval(()=>{
+     /*  clearTimeout(miTimeout) */
+      const miTimeout = setTimeout(()=>{
          
-        setCounter1((prevCount) => prevCount + 1 );
+         
+         if(counterHours1 === 9 && counterMinutes2 === 9 && counterMinutes1 === 9 && counterSeconds2 === 5 && counterSeconds1 === 9 ){
+          
+             setCounter1(0)
+             setCounter2(0)
+             setCounter3(0)
+             setCounter4(0)
+             setCounter5(0)
+             setCounter6(counterHours1 + 1)
+             return
+            }
+         
+         
+         
+         if(counterMinutes2 === 9 && counterMinutes1 === 9 && counterSeconds2 === 5 && counterSeconds1 === 9 ){
+          
+             setCounter1(0)
+             setCounter2(0)
+             setCounter3(0)
+             setCounter4(0)
+             setCounter5(counterHours1 + 1)
+             return
+            }
+         
+         if(counterMinutes1 === 9 && counterSeconds2 === 5 && counterSeconds1 === 9 ){
+          
+             setCounter1(0)
+             setCounter2(0)
+             setCounter3(0)
+             setCounter4(counterMinutes2 + 1)
+             return
+            }
  
-      },200)
-      return () => clearInterval(miTimeout)
-    },[]) 
+        if(counterSeconds2 === 5 && counterSeconds1 === 9 ){
+         setCounter1(0)
+         setCounter2(0)
+         setCounter3(counterMinutes1 + 1)
+         return
+        }
+ 
+         if(counterSeconds1 === 9){
+             setCounter1(0)
+             setCounter2(counterSeconds2 + 1)
+             return
+           }
+ 
+ 
+ 
+ 
+ 
+       
+        setCounter1(counterSeconds1 + 1);
+      },1000)
+    },[counterSeconds1])
         
  
  
- useEffect(() => {
- 
-   if(counterHours2 === 2 && counterHours1 === 4 && counterMinutes2 === 5 && counterMinutes1 === 9 && counterSeconds2 === 5 && counterSeconds1 === 9 ){
- 
-     setCounter1(0)
-     setCounter2(0)
-     setCounter3(0)
-     setCounter4(0)
-     setCounter5(0)
-     setCounter6(0)
-     return {handleReset}
-    }
- 
- 
-   if(counterHours1 === 9 && counterMinutes2 === 5 && counterMinutes1 === 9 && counterSeconds2 === 5 && counterSeconds1 === 9 ){
- 
-     setCounter1(0)
-     setCounter2(0)
-     setCounter3(0)
-     setCounter4(0)
-     setCounter5(0)
-     setCounter6((prevCount) => prevCount +1)
-     return
-    }
- 
- 
- 
- if(counterMinutes1 === 9 && counterMinutes2 === 5 &&  counterSeconds1 === 9 && counterSeconds2 === 5){
- 
-     setCounter1(0)
-     setCounter2(0)
-     setCounter3(0)
-     setCounter4(0)
-     setCounter5((prevCount) => prevCount +1)
-     return
-    } 
- 
- if(counterMinutes1 === 9 && counterSeconds2 === 5 && counterSeconds1 === 9 ){
- 
-     setCounter1(0)
-     setCounter2(0)
-     setCounter3(0)
-     setCounter4((prevCount) => prevCount +1)
-     return
-    }
- 
-   if(counterSeconds2 === 5 && counterSeconds1 === 9){
-     setCounter1(0)
-     setCounter2(0)
-     setCounter3((prevCount) => prevCount +1)
-     return
-   }
- 
-   if(counterSeconds1 === 10){
-     setCounter1(0)
-     setCounter2((prevCount) => prevCount +1 )
-     return
-   }
- }, [counterSeconds1]) 
- 
- useEffect(()=> console.log("actualizacion")) 
- 
- const handleReset = () => {setCounter1(0)
-   setCounter2(0)
-   setCounter3(0)
-   setCounter4(0)
-   setCounter5(0)
-   setCounter6(0)
-   }
    
- 
- 
- 
  
      return (
      <div className="container-fluid p-5">
@@ -104,41 +81,41 @@ function Card(){
  
          <div className="card bg-dark text-primary me-2">
          <div className="card-body">
-           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterHours2}</h5>
+           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterHours2.toString()}</h5>
          </div>
          </div>
  
          <div className="card bg-dark text-primary me-2">
          <div className="card-body">
-           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterHours1}</h5>
+           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterHours1.toString()}</h5>
          </div>
          </div>
  
          <div className="card bg-dark text-warning me-2">
          <div className="card-body">
-           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterMinutes2}</h5>
+           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterMinutes2.toString()}</h5>
          </div>
          </div>
  
          <div className="card bg-dark text-warning me-2">
          <div className="card-body">
-           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterMinutes1}</h5>
+           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterMinutes1.toString()}</h5>
          </div>
          </div>
  
          <div className="card bg-dark text-danger me-2">
          <div className="card-body">
-           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterSeconds2}</h5>
+           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterSeconds2.toString()}</h5>
          </div>
          </div>
  
          <div className="card bg-dark text-danger me-2">
          <div className="card-body">
-           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterSeconds1}</h5>
+           <h5 className="card-title position-absolute top-50 start-50 translate-middle fs-1">{counterSeconds1.toString()}</h5>
          </div>
          </div>
          </div>
-         <a href="#" onClick={handleReset} className="btn btn-success d-flex justify-content-center mt-3">Reset Button</a>
+         <a href="#" onClick={() => setCounter1(0)} className="btn btn-success d-flex justify-content-center mt-3">Reset Button</a>
      </div>
      );
  
